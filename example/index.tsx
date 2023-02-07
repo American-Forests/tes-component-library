9efaea0562b71f674ad9667ccea0a2b63d3740d9
Chase Dawson <scd3dz@virginia.edu>
2023-02-07 17:20:13 -0500

    example app with tes-component-library working

--- example/index.tsx
+++ example/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Button } from 'tes-component-library';
+import 'tes-component-library/dist/tailwind.css';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Button text="Click me"></Button>
+      <Button text="Cancel" variant="secondary"></Button>
     </div>
   );
 };
